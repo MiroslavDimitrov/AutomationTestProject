5da615b2c94eed0a66735d9e3d5e62f84d74ea84
Miroslav Todorov <miroslav.dim.todorov@gmail.com>
2025-06-07 19:15:19 +0300

    Add project files.

---
+++ BASE/DriverFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationTestProject
+{
+    internal class DriverFactory
+    {
+        public static IWebDriver GetDriver(string browser)
+        {
+            switch (browser.ToLower())
+            {
+                case "chrome":
+                    return new ChromeDriver();
+                case "firefox":
+                    return new FirefoxDriver();
+                case "edge":
+                    return new EdgeDriver();
+                default:
+                    throw new ArgumentException($"Browser '{browser}' is not supported.");
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3c1cda40-66b0-463e-83cd-b17870d9c02d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
